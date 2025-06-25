18ad30c99de92c89b8689d11125788b390d43fc3
pduartedev <patrick.d.pimenta@gmail.com>
2025-06-25 07:53:08 -0300

    feat: doc/FlexEntregas_Polimorfismo.pdf

--- src/SimuladorDeEntrega.php
+++ src/SimuladorDeEntrega.php
@@ -1,22 +1,26 @@
 <?php
 require_once __DIR__ . '/Interfaces/MeioDeEntregaInterface.php';
 
-class SimuladorDeEntrega {
+class SimuladorDeEntrega
+{
     private float $distancia;
     private string $clima;
 
-    public function __construct(float $distancia, string $clima) {
+    public function __construct(float $distancia, string $clima)
+    {
         $this->distancia = $distancia;
         $this->clima = $clima;
     }
 
-    public function simularTodos(array $meios): void {
+    public function simularTodos(array $meios): void
+    {
         foreach ($meios as $meio) {
             $this->simular($meio);
         }
     }
 
-    private function simular(MeioDeEntregaInterface $meio): void {
+    private function simular(MeioDeEntregaInterface $meio): void
+    {
         echo "== {$meio->getNome()} ==\n";
         if (!$meio->podeOperar($this->clima)) {
             echo " Não pode operar no clima: {$this->clima}\n\n";
@@ -25,6 +29,7 @@ class SimuladorDeEntrega {
 
         $tempo = $meio->calcularTempoEntrega($this->distancia, $this->clima);
         $custo = $meio->calcularCustoEntrega($this->distancia);
+        
         echo " Tempo estimado: " . round($tempo, 2) . " horas\n";
         echo " Custo estimado: R$ " . number_format($custo, 2, ',', '.') . "\n\n";
     }
